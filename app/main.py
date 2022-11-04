95f54bf3fd65968a1976a084f7a9b01f8ee24b3b
Yegor Stepanov <yegors@mustardsystems.com>
2022-11-04 00:12:16 +0000

    add migrations skeleton

--- app/main.py
+++ app/main.py
@@ -1,11 +1,36 @@
 import configparser
 import argparse
+import asyncio
 
-CONFIG = configparser.ConfigParser()
+from utils.db_handler import DBHandler
 
-if __name__ == '__main__':
+
+async def main(loop):
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, required=True)
     args = parser.parse_args()
-    CONFIG.read(args.config)
-    print(CONFIG.get('db', 'host'))
+    config = configparser.ConfigParser()
+    config.read(args.config)
+
+    db_host = config.get('db', 'host')
+    db_user = config.get('db', 'user')
+    db_password = config.get('db', 'password')
+    db_name = config.get('db', 'name')
+    db = DBHandler(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        name=db_name,
+        loop=loop,
+    )
+    await db.setup()
+
+    await db.test_mysql()
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(main(loop))
+    finally:
+        loop.close()
