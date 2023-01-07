da66359d442d08eed0737f957572e3bcc3bfdc14
Yegor Stepanov <ystepanoff@icloud.com>
2023-01-07 12:02:32 +0000

    main-patch-2: fix pylint suggestions (ignore C0114, C0115, C0116, and R0903 for now)

--- app/main.py
+++ app/main.py
@@ -21,10 +21,10 @@ def has_updated(new_state: str, old_state: str) -> bool:
     return new_hash != old_hash
 
 
-async def process_source(db: DBHandler, source: Dict[str, Any]) -> None:
+async def process_source(db_handler: DBHandler, source: Dict[str, Any]) -> None:
     source_id = int(source['id'])
-    old_state = await db.latest_state(source_id)
-    scraper_data = await db.find_scraper(source_id)
+    old_state = await db_handler.latest_state(source_id)
+    scraper_data = await db_handler.find_scraper(source_id)
     if scraper_data is not None:
         try:
             scraper = getattr(scrapers, scraper_data['base_class'])(
@@ -33,17 +33,17 @@ async def process_source(db: DBHandler, source: Dict[str, Any]) -> None:
             )
             new_state = await scraper.scrape()
             if has_updated(new_state, old_state['data']):
-                actions_data = await db.list_actions(source_id)
+                actions_data = await db_handler.list_actions(source_id)
                 for action_data in actions_data:
                     action = getattr(actions, action_data['base_class'])(**action_data['params'])
                     await action.action(
                         meta='Updated: {}'.format(source['remote']),
                         message='Updated: {}'.format(source['remote']),
                     )
-                await db.upsert_state(source_id, new_state)
+                await db_handler.upsert_state(source_id, new_state)
         except AttributeError:
             traceback.print_exc()
-            logging.error('Wrong scraper class %s for source %s.', scraper_data['base_class'], source_id)
+            logging.error('Wrong scraper %s for source %s.', scraper_data['base_class'], source_id)
     else:
         logging.error('Could not find any scrapers for source %s.', source_id)
 
@@ -66,7 +66,7 @@ async def main() -> None:
         logging.error('Cannot obtain lock — is the app already running?')
         sys.exit(0)
 
-    db = DBHandler(
+    db_handler = DBHandler(
         params=DBParams(
             host=config.get('db', 'host'),
             user=config.get('db', 'user'),
@@ -74,17 +74,17 @@ async def main() -> None:
             name=config.get('db', 'name'),
         ),
     )
-    await db.setup()
+    await db_handler.setup()
 
-    sources = await db.list_sources()
+    sources = await db_handler.list_sources()
     tasks = [
         asyncio.create_task(
-            process_source(db, source)
+            process_source(db_handler, source)
         ) for source in sources
     ]
     await asyncio.gather(*tasks)
 
-    await db.destroy()
+    await db_handler.destroy()
     lock.close()
 
 
