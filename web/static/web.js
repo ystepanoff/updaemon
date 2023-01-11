d030179ccdf7e2a9175d9d71478852ded4a62549
Yegor Stepanov <ystepanoff@icloud.com>
2023-01-11 20:59:43 +0000

    DEV-3: Fix source delete bug

--- web/static/web.js
+++ web/static/web.js
@@ -120,7 +120,7 @@ $(document).ready(function() {
                     "/source",
                     {
                         "source_id": source_id,
-                        "detele": true
+                        "delete": true
                     },
                     () => location.reload()
                 ).fail(function (xhr, status, error) {
