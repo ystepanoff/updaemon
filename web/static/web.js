9b1f7cf935ee839f94d8bb4a816d47a6848d9e90
Yegor Stepanov <ystepanoff@icloud.com>
2023-02-12 16:56:00 +0000

    dev-8: fixes from testing

--- web/static/web.js
+++ web/static/web.js
@@ -208,12 +208,32 @@ $(document).ready(function() {
         addActionButton.click(function () {
             modal.addClass('is-active');
         });
+        $('#addActionBaseClass').change(function () {
+            for (let actionParamsTable of $('[id^="addActionParamsTable-"]')) {
+                let parts = actionParamsTable.id.split('-');
+                let baseClassId = parseInt(parts[1]);
+                if (baseClassId === parseInt($('#addActionBaseClass').val())) {
+                    $('#' + actionParamsTable.id).removeClass('is-hidden');
+                } else {
+                    $('#' + actionParamsTable.id).addClass('is-hidden');
+                }
+            }
+        });
         modal.find('#addActionSave').click(function () {
             let baseClassId = parseInt(modal.find('#addActionBaseClass').val());
             let paramsTable = modal.find('#addActionParamsTable-' + baseClassId);
+            let paramsConfig = {};
+            for (let paramConfig of paramsTable.find('[id^="addActionParamConfig-"]')) {
+                let parts = paramConfig.id.split('-');
+                paramsConfig[parts[1]] = paramConfig.value;
+            }
             let params = {};
             for (let inputField of paramsTable.find(':input')) {
-                params[inputField.name] = inputField.value;
+                if (inputField.type !== "hidden" && paramsConfig[inputField.name] === "list") {
+                    params[inputField.name] = inputField.value.replace(/\s/g, '').split(',');
+                } else {
+                    params[inputField.name] = inputField.value;
+                }
             }
             $.post(
                 "/action",
